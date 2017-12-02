2395f8435aa5de1b90b3e7e3e2bcc16b116f1ff5
Zoli <vz1018@gmail.com>
2017-12-02 16:49:22 +0100

    fix warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
    /**
-   * This is is a class reurn with hello
+   * @param someone the name of the person
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
